baaa193ecc6f9a33624e981c40076d39b98d3e6c
theclouday <theclouday@example.com>
2024-05-03 21:32:51 +0200

    added a custom handler exception

---
+++ src/main/java/ua/assignmentTwo/webService/exceptions/CustomExceptionHandler.java
@@ -0,0 +1,19 @@
+package ua.assignmentTwo.webService.exceptions;
+
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
+public class CustomExceptionHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public final ResponseEntity<Object> handleBadRequestException(RequiredParameterIsEmptyException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
