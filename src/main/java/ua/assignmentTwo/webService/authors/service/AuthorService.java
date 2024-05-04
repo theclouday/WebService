8b49ea72821710b35f1b089794ed88c19673b62b
theclouday <theclouday@example.com>
2024-05-04 14:17:05 +0200

    bugs fixed

--- src/main/java/ua/assignmentTwo/webService/authors/service/AuthorService.java
+++ src/main/java/ua/assignmentTwo/webService/authors/service/AuthorService.java
@@ -1,13 +1,14 @@
 package ua.assignmentTwo.webService.authors.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import ua.assignmentTwo.webService.authors.dto.AuthorUpdateDto;
 import ua.assignmentTwo.webService.authors.dto.AuthorCreateDto;
 import ua.assignmentTwo.webService.authors.model.Author;
 import ua.assignmentTwo.webService.authors.repository.AuthorRepository;
+import ua.assignmentTwo.webService.exceptions.RequiredParameterIsDuplicateException;
 
-import javax.swing.text.html.parser.Entity;
 import java.util.List;
 
 @Service
@@ -25,15 +26,37 @@ public class AuthorService {
         author.setId(authorCreateDto.getId());
         author.setName(authorCreateDto.getName());
         author.setSurname(authorCreateDto.getSurname());
+        uniquenessOfValues(author);
         authorRepository.save(author);
     }
 
     public void updateDataInAuthor(Long id, AuthorUpdateDto authorUpdateDto) {
         Author authorToUpdate = authorRepository.findAllById(id);
 
+        uniquenessOfValues(authorUpdateDto);
         authorToUpdate.setName(authorUpdateDto.getName());
         authorToUpdate.setSurname(authorUpdateDto.getSurname());
         authorRepository.save(authorToUpdate);
+    }
+
+    @SneakyThrows
+    private void uniquenessOfValues(Author author) {
+        checkAuthorUniqueness(author.getName(), author.getSurname());
+    }
+
+    @SneakyThrows
+    private void uniquenessOfValues(AuthorUpdateDto authorUpdateDto) {
+        checkAuthorUniqueness(authorUpdateDto.getName(), authorUpdateDto.getSurname());
+    }
+
+    private void checkAuthorUniqueness(String name, String surname) {
+        List<Author> authors = authorRepository.findAll();
+
+        for (Author author : authors) {
+            if (name.equals(author.getName()) && surname.equals(author.getSurname())) {
+                throw new RequiredParameterIsDuplicateException("Field Name or Surname is duplicate");
+            }
+        }
     }
 
     public void deleteAuthorById(Long id) {
